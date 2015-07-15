24326abd0ba7229662a491d4ec12412a4b7851e9
catcher1 <danlau168@gmail.com>
2015-07-14 17:56:39 -0700

    Admins can edit project roles of existing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :archive]
+    before_action :set_user, only: [:show, :edit, :update, :archive]
+  before_action :set_projects, only: [:new, :create, :edit, :update]
   def index
       @users = User.excluding_archived.order(:email)
   end
@@ -29,14 +30,21 @@ class Admin::UsersController < Admin::ApplicationController
     if params[:user][:password].blank?
       params[:user].delete(:password)
     end
-    
-    if @user.update(user_params)
-      flash[:notice] = "User has been updated."
-      redirect_to admin_users_path
-    else
-      flash.now[:alert] = "User has not been updated."
-      render "edit"
-    end
+    User.transaction do
+      @user.roles.clear
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+  end end
+      if @user.update(user_params)
+        flash[:notice] = "User has been updated."
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "User has not been updated."
+        render "edit"
+        raise ActiveRecord::Rollback
+  end end
   end
   
   def archive
@@ -50,6 +58,9 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
 private
+def set_projects
+    @projects = Project.order(:name)
+end
   def user_params
     params.require(:user).permit(:email, :password, :admin)
   end
